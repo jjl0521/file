a73548934ad8f0c495dc9f12d21a4c5ff9b790d5
jjl0521 <88264204+jjl0521@users.noreply.github.com>
2021-08-15 15:09:08 +0900

    시험

---
+++ 0815.py
@@ -0,0 +1,10 @@
+
+for i in range(1,11):
+    if i%2== 0:
+        print(i*(-1))
+    else:
+        print(i)
+my_list =["a","b","c","d","e"]
+print(my_list[3:])
+my_list =[1,2,3,4,5]
+print(my_list[:5]
